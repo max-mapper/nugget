fba4696a274d2142ef6a674c62f7bf270c8097ae
Max Ogden <max@maxogden.com>
2016-02-09 21:10:48 -0800

    add args to usage

--- bin.js
+++ bin.js
@@ -5,14 +5,12 @@ var args = require('minimist')(process.argv.slice(2))
 
 var urls = args._
 if (urls.length === 0) {
-  console.error(
     "Usage: nugget <urls> [options]\n" +
     "  -o    output filename\n" +
     "  -d    output parent directory\n" +
     "  -c    resume aborted download\n" +
     "  -f    ignore response codes > 299\n" +
     "  -s    concurrent socket limit (default infinity)"
-  )
   process.exit(1)
 }
 
